296f2574ae8259f47f3095c0f40250a4b8b239f0
Beni <vescienblake@gmail.com>
2026-08-26 16:53:16 -0300

    Add function to display book

--- script.js
+++ script.js
@@ -4,13 +4,24 @@ const title = document.querySelector("#title");
 const author = document.querySelector("#author");
 const pages = document.querySelector("#pages");
 const addBook = document.querySelector(".add-book");
+const shelf = document.querySelector(".shelf");
 
+add.addEventListener("click", () => {
+    form.style.display = "block";
+})
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        form.style.display = "none";
+    }
+})
 
 addBook.addEventListener("click", (event) => {
     event.preventDefault();
     if (title.value && author.value && pages.value) {
         addBookToLibrary(title.value, author.value, pages.value);
-        console.log(myLibrary);
+        displayBook(title.value, author.value, pages.value);
+        form.style.display = "none";
     }
 })
 
@@ -27,19 +38,33 @@ function addBookToLibrary(title, author, pages) {
     myLibrary.push(book);
 }
 
-function showHideForm() {
-    add.addEventListener("click", () => {
-        form.style.display = "block";
-    })
+function displayBook(title, author, pages) {
+    const book = document.createElement("div");
+    book.classList.add("book");
 
-    document.addEventListener("keydown", (event) => {
-        if (event.key === "Escape") {
-            form.style.display = "none";
-        }
-    })
+    const headerTitle = document.createElement("p");
+    const headerAuthor = document.createElement("p");
+    const headerPages = document.createElement("p");
+    const bookTitle = document.createElement("p");
+    const bookAuthor = document.createElement("p");
+    const bookPages = document.createElement("p");
+
+    headerTitle.textContent = "Title";
+    headerAuthor.textContent = "Author";
+    headerPages.textContent = "Pages";
+    bookTitle.textContent = title;
+    bookAuthor.textContent = author;
+    bookPages.textContent = pages;
+
+    book.append(headerTitle, bookTitle, document.createElement("hr"));
+    book.append(headerAuthor, bookAuthor, document.createElement("hr"));
+    book.append(headerPages, bookPages);
+
+    shelf.appendChild(book);
 }
 
-showHideForm();
+
+
 
 
 
