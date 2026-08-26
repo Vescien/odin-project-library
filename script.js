a4d7cb1d4153a55f9f1ac65bd65c4f9de8c9d177
Beni <vescienblake@gmail.com>
2026-08-26 16:01:26 -0300

    Add logic to add book to list

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const add = document.querySelector(".add");
+const form = document.querySelector(".form-container");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const addBook = document.querySelector(".add-book");
+
+
+addBook.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (title.value && author.value && pages.value) {
+        addBookToLibrary(title.value, author.value, pages.value);
+        console.log(myLibrary);
+    }
+})
+
 const myLibrary = [];
 
 function Book(title, author, pages) {
@@ -11,6 +27,19 @@ function addBookToLibrary(title, author, pages) {
     myLibrary.push(book);
 }
 
-addBookToLibrary("George R. R. Martin", "A Song of Ice and Fire", "300");
-console.log(myLibrary);
+function showHideForm() {
+    add.addEventListener("click", () => {
+        form.style.display = "block";
+    })
+
+    document.addEventListener("keydown", (event) => {
+        if (event.key === "Escape") {
+            form.style.display = "none";
+        }
+    })
+}
+
+showHideForm();
+
+
 
