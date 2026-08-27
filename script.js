631141b5986d86f948200c313087bd8437fc6598
Beni <vescienblake@gmail.com>
2026-08-27 12:56:38 -0300

    Refactor displayBook function

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const author = document.querySelector("#author");
 const pages = document.querySelector("#pages");
 const addBook = document.querySelector(".add-book");
 const shelf = document.querySelector(".shelf");
-const deleteButton = document.querySelector(".delete");
 
 const myLibrary = [
     { title: 'The Hobbit', author: 'J. R. R. Tolkien', pages: '200', id: '17a29217-a893-4072-a656-2b5df81e6c4f' },
@@ -32,37 +31,26 @@ function getBook(library) {
 }
 
 function displayBook(title, author, pages, id) {
-    const bookContainer = document.createElement("div");
-    const book = document.createElement("div");
-    bookContainer.classList.add("book-container");
-    book.classList.add("book");
-    bookContainer.dataset.id = id;
+    const bookHTML = `
+        <div class="book-container" data-id="${id}">
+            <div class="book">
+                <p>Title</p>
+                <p>${title}</p>
+                <hr>
 
-    const headerTitle = document.createElement("p");
-    const headerAuthor = document.createElement("p");
-    const headerPages = document.createElement("p");
-    const bookTitle = document.createElement("p");
-    const bookAuthor = document.createElement("p");
-    const bookPages = document.createElement("p");
-    const deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("delete");
-
-    headerTitle.textContent = "Title";
-    headerAuthor.textContent = "Author";
-    headerPages.textContent = "Pages";
-    bookTitle.textContent = title;
-    bookAuthor.textContent = author;
-    bookPages.textContent = pages;
-    deleteBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" /></svg>`
-
-    book.append(headerTitle, bookTitle, document.createElement("hr"));
-    book.append(headerAuthor, bookAuthor, document.createElement("hr"));
-    book.append(headerPages, bookPages);
-    bookContainer.append(book);
-    bookContainer.append(deleteBtn);
-    
-    shelf.appendChild(bookContainer);
-    console.log(id);
+                <p>Author</p>
+                <p>${author}</p>
+                <hr>
+                
+                <p>Pages</p>
+                <p>${pages}</p>
+            </div>
+            <button class="delete">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" /></svg>
+            </button>
+        </div>
+    `;
+    shelf.insertAdjacentHTML("beforeend", bookHTML);
 }
 
 add.addEventListener("click", () => {
