10ac3f84a01338073e2cccbbb1cb032d4b5a58bf
Beni <vescienblake@gmail.com>
2026-08-27 08:55:25 -0300

    Add delete button (visual only)

--- script.js
+++ script.js
@@ -34,8 +34,11 @@ function getBook(library) {
 }
 
 function displayBook(title, author, pages) {
+    const bookContainer = document.createElement("div");
     const book = document.createElement("div");
+    bookContainer.classList.add("book-container");
     book.classList.add("book");
+
 
     const headerTitle = document.createElement("p");
     const headerAuthor = document.createElement("p");
@@ -43,6 +46,8 @@ function displayBook(title, author, pages) {
     const bookTitle = document.createElement("p");
     const bookAuthor = document.createElement("p");
     const bookPages = document.createElement("p");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete");
 
     headerTitle.textContent = "Title";
     headerAuthor.textContent = "Author";
@@ -50,12 +55,15 @@ function displayBook(title, author, pages) {
     bookTitle.textContent = title;
     bookAuthor.textContent = author;
     bookPages.textContent = pages;
+    deleteBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Delete</title><path d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" /></svg>`
 
     book.append(headerTitle, bookTitle, document.createElement("hr"));
     book.append(headerAuthor, bookAuthor, document.createElement("hr"));
     book.append(headerPages, bookPages);
-
-    shelf.appendChild(book);
+    bookContainer.append(book);
+    bookContainer.append(deleteBtn);
+    
+    shelf.appendChild(bookContainer);
 }
 
 add.addEventListener("click", () => {
