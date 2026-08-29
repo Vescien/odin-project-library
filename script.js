4dc1524dd8ed245caa2ea750a5a10a0c31a23b64
Beni <vescienblake@gmail.com>
2026-08-29 13:11:37 -0300

    Refactor displayBook again
    Refactored displayBook function again so it would work with checking if
    a book is already read or not, before it was only doing it visually and
    now it adds a boolean to check if the book is already marked as read
    which lets books already in the library be marked correctly when the page
    first loads

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ const pages = document.querySelector("#pages");
 const addBook = document.querySelector(".add-book");
 const shelf = document.querySelector(".shelf");
 const formInputs = document.querySelector("form");
-const bookStatus = document.querySelector("#status");
+const formStatus = document.querySelector("#status");
 
 const myLibrary = [
     { title: 'The Hobbit', author: 'J. R. R. Tolkien', pages: '200', id: '17a29217-a893-4072-a656-2b5df81e6c4f', status: true },
     { title: 'A Song of Ice and Fire', author: 'George R. R. Martin', pages: '300', id: 'cc39c743-221b-4a3e-8d99-6f6cdd243563', status: false },
- ];
+ ]
 
 function Book(title, author, pages, id, status) {
     this.title = title;
@@ -35,34 +35,56 @@ function getBook(library) {
 }
 
 function displayBook(book) {
-    const bookHTML = `
-        <div class="book-container" data-id="${book.id}">
-            <div class="book">
-                <p>Title</p>
-                <p class="book-text">${book.title}</p>
-                <hr>
+    const bookContainer = document.createElement("div");
+    const bookLayout = document.createElement("div");
+    const statusContainer = document.createElement("div");
+    const deleteBtn = document.createElement("button");
+    bookContainer.classList.add("book-container");
+    bookLayout.classList.add("book");
+    statusContainer.classList.add("status-container");
+    deleteBtn.classList.add("delete");
 
-                <p>Author</p>
-                <p class="book-text">${book.author}</p>
-                <hr>
-                
-                <p>Pages</p>
-                <p class="book-text">${book.pages}</p>
-            </div>
-            <button class="delete">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" /></svg>
-            </button>
-            <label class="checkbox-container">
-                <input class="read" type="checkbox" />
-                <span class="button-label">
-                <svg class="icon-checked" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0.41,13.41L6,19L7.41,17.58L1.83,12M22.24,5.58L11.66,16.17L7.5,12L6.07,13.41L11.66,19L23.66,7M18,7L16.59,5.58L10.24,11.93L11.66,13.34L18,7Z" /></svg>
+    const headerTitle = document.createElement("p");
+    const headerAuthor = document.createElement("p");
+    const headerPages = document.createElement("p");
+    const bookTitle = document.createElement("p");
+    const bookAuthor = document.createElement("p");
+    const bookPages = document.createElement("p");
 
-                <p class="icon-unchecked">Unread</p>
-                </span>
-            </label>
-        </div>
-    `;
-    shelf.insertAdjacentHTML("beforeend", bookHTML);
+    headerTitle.textContent = "Title";
+    headerAuthor.textContent = "Author";
+    headerPages.textContent = "Pages";
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    bookPages.textContent = book.pages;
+    bookContainer.dataset.id = book.id;
+    statusContainer.innerHTML = bookStatusStyle(bookContainer, book.status);
+    deleteBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" /></svg>`;
+
+    statusContainer.addEventListener("click", () => {
+        book.status = !book.status;
+        console.log(book.title, book.status);
+        statusContainer.innerHTML = bookStatusStyle(statusContainer, book.status);
+    });
+
+    bookLayout.append(headerTitle, bookTitle, document.createElement("hr"));
+    bookLayout.append(headerAuthor, bookAuthor, document.createElement("hr"));
+    bookLayout.append(headerPages, bookPages);
+    bookContainer.append(bookLayout);
+    bookContainer.append(deleteBtn);
+    bookContainer.append(statusContainer);
+
+    shelf.appendChild(bookContainer);
+}
+
+function bookStatusStyle(statusContainer, formStatus) {
+    if (formStatus === true) {
+        statusContainer.style.backgroundColor = "#00ff40";
+        return `<svg xmlns = "http://www.w3.org/2000/svg" viewBox = "0 0 24 24" ><title>check-all</title><path d="M0.41,13.41L6,19L7.41,17.58L1.83,12M22.24,5.58L11.66,16.17L7.5,12L6.07,13.41L11.66,19L23.66,7M18,7L16.59,5.58L10.24,11.93L11.66,13.34L18,7Z" /></svg >`;
+    } else {
+        statusContainer.style.backgroundColor = "#ff1010";
+        return "Unread";
+    }
 }
 
 add.addEventListener("click", () => {
@@ -81,7 +103,7 @@ addBook.addEventListener("click", (event) => {
     event.preventDefault();
     if (title.value && author.value && pages.value) {
         let newID = crypto.randomUUID();
-        const newBook = addBookToLibrary(title.value, author.value, pages.value, newID, bookStatus.checked);
+        const newBook = addBookToLibrary(title.value, author.value, pages.value, newID, formStatus.checked);
         displayBook(newBook);
         formInputs.reset();
         form.style.display = "none";
@@ -101,10 +123,7 @@ shelf.addEventListener("click", (event) => {
         bookToRemove.remove();
         console.log(myLibrary);
     }
-});
+})
 
 getBook(myLibrary);
 console.log(myLibrary);
-
-
-
